abb8b417e5af626028a4f6ace8aa996ca41a7908
Agvp1521 <agvp1521@gmail.com>
2025-04-29 12:15:04 -0500

    master

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,50 @@
+const CACHE_NAME = 'misiones-limpieza-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.ico',
+  './logo192.png',
+  './logo512.png',
+  './static/js/main.b71dc500.js',
+  './static/css/main.c44df42a.css'
+];
+
+// Instalar y guardar archivos en caché
 self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado.');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activado.');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
-  });
-  
+  console.log('[Service Worker] Instalando y cacheando archivos');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activar y limpiar versiones antiguas de caché
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activado');
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[Service Worker] Borrando caché antigua:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Interceptar peticiones y responder desde caché si está disponible
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Si está en caché, responde desde allí, si no, busca online
+      return response || fetch(event.request);
+    })
+  );
+});
